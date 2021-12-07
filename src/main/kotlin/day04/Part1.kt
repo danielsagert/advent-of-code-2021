b2da34a440cc8a8efb8620326715f1c4bd0e878d
Sagert, Daniel (extern) <daniel.sagert.extern@prosiebensat1.com>
2021-12-07 19:44:33 +0100

    Day 4 part 1

---
+++ src/main/kotlin/day04/Part1.kt
@@ -0,0 +1,60 @@
+package day04
+
+
+object Part1 {
+    fun result(input: String): Int {
+        val lines = input.lines().filter { it.isNotBlank() }
+        val numbers = extractNumbers(lines)
+        val boards = extractBoards(lines)
+        val combosOfAllBoards = combos(boards)
+
+        for (i in 5..numbers.size) {
+            val activeNumbers = numbers.subList(0, i - 1)
+
+            for ((boardIndex, comboOfSingleBoard) in combosOfAllBoards.withIndex()) {
+                for (combo in comboOfSingleBoard) {
+                    if (activeNumbers.containsAll(combo)) {
+                        val board = boards[boardIndex]
+                        val unmarkedSum = sumOfUnmarked(board, activeNumbers)
+                        val lastNumber = activeNumbers.last()
+                        return unmarkedSum * lastNumber
+                    }
+                }
+            }
+        }
+
+        return 0
+    }
+
+    private fun extractNumbers(lines: List<String>) = lines[0]
+        .split(",")
+        .map { it.toInt() }
+
+    private fun extractBoards(lines: List<String>) = lines
+        .drop(1)
+        .chunked(5)
+        .map { boardLines ->
+            List(5) { i ->
+                boardLines[i].trim().split("\\s+".toRegex()).map { it.toInt() }.toList()
+            }
+        }
+
+    private fun combos(boards: List<List<List<Int>>>) = boards
+        .map { winRows ->
+            val winCols = List(5) { mutableListOf<Int>() }
+            for (i in 0..4) {
+                for (j in 0..4) {
+                    winCols[j].add(i, winRows[i][j])
+                }
+            }
+            winRows.plus(winCols)
+        }
+
+    private fun sumOfUnmarked(board: List<List<Int>>, activeNumbers: List<Int>) =
+        board.flatten().filterNot(activeNumbers::contains).sum()
+}
+
+fun main() {
+    val input = FileReader.content("/day04/input.txt")
+    println(Part1.result(input))
+}
