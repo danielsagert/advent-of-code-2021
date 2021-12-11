d4f145ef4eb1fdba692247a9a458c30840203706
Sagert, Daniel (extern) <daniel.sagert.extern@prosiebensat1.com>
2021-12-11 22:51:33 +0100

    Day 5 part 2

---
+++ src/main/kotlin/day05/Part2.kt
@@ -0,0 +1,67 @@
+package day05
+
+import FileReader
+
+object Part2 {
+    data class Position(val x: Int, val y: Int)
+
+    fun result(input: String): Int {
+        val lines = input.lines()
+            .filter(String::isNotBlank)
+            .map(::toPositionPair)
+        val allPositions = lines.flatMap { it.toList() }
+        val maxX = allPositions.maxOfOrNull(Position::x)?.let { it + 1 } ?: 0
+        val maxY = allPositions.maxOfOrNull(Position::y)?.let { it + 1 } ?: 0
+        val diagram = Array(maxY) { IntArray(maxX) { 0 } }
+
+        for (line in lines) {
+            val xSteps = toSteps(line.first.x, line.second.x)
+            val ySteps = toSteps(line.first.y, line.second.y)
+
+            val points = if (xSteps.size == 1) {
+                // vertical
+                val x = xSteps.first()
+                ySteps.map { Pair(x, it) }
+            } else if (ySteps.size == 1) {
+                // horizontal
+                val y = ySteps.first()
+                xSteps.map { Pair(it, y) }
+            } else {
+                //diagonal
+                xSteps.zip(ySteps)
+            }
+
+            for (point in points) {
+                val x = point.first
+                val y = point.second
+                diagram[y][x] = diagram[y][x] + 1
+            }
+        }
+
+        return diagram.flatMap { it.asIterable() }.count { it > 1 }
+    }
+
+    private fun toPositionPair(line: String): Pair<Position, Position> {
+        val coordinates = line.split("->")
+        return Pair(toPosition(coordinates.first()), toPosition(coordinates.last()))
+    }
+
+    private fun toPosition(coordinates: String): Position {
+        val points = coordinates.split(",")
+        val x = points.first().trim().toInt()
+        val y = points.last().trim().toInt()
+        return Position(x, y)
+    }
+
+    private fun toSteps(from: Int, to: Int) =
+        if (from <= to) {
+            (from..to).toList()
+        } else {
+            (from downTo to).toList()
+        }
+}
+
+fun main() {
+    val input = FileReader.content("/day05/input.txt")
+    println(Part2.result(input))
+}
