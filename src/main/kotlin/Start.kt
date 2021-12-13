e367ede4409336fa9b3e03fd84d5767171588e64
Sagert, Daniel (extern) <daniel.sagert.extern@prosiebensat1.com>
2021-12-13 12:57:27 +0100

    Measure time of puzzle execution

--- src/main/kotlin/Start.kt
+++ src/main/kotlin/Start.kt
@@ -1,8 +1,12 @@
 import day06.Day06Part1
+import kotlin.system.measureTimeMillis
 
 fun content(filePath: String) = object {}.javaClass.getResource(filePath)?.readText().orEmpty()
 
 fun main() {
     val input = content("/day06/input.txt")
-    println(Day06Part1.result(input))
+    val duration = measureTimeMillis {
+        println("Result: ${Day06Part1.result(input)}")
+    }
+    println("Duration: $duration ms")
 }
